5b70c34bef988f8bd8198bfabc02e16ead43287a
Sergio Pomárico <sergiodavid21@gmail.com>
2022-10-17 20:23:33 -0500

    add git ignore

--- lua/settings.lua
+++ lua/settings.lua
@@ -9,3 +9,11 @@ set.number = true
 
 -- Highlights code
 set.termguicolors = true
+
+-- Tabs & indents
+opt.autoindent = true
+opt.breakindent = true
+opt.expandtab = true
+opt.smarttab = true
+opt.smartindent = true
+opt.tabstop = 2
