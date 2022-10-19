ae62a1ec967178012e4005305e8349d6041b3347
Sergio Pomárico <sergiodavid21@gmail.com>
2022-10-18 23:49:33 -0500

    add lsp

--- lua/settings.lua
+++ lua/settings.lua
@@ -17,3 +17,11 @@ set.expandtab = true
 set.smarttab = true
 set.smartindent = true
 set.tabstop = 2
+set.shiftwidth = 2
+
+-- Highlights code
+set.cursorline = true
+set.winblend = 0
+set.wildoptions = 'pum'
+set.pumblend = 5
+set.background = 'dark'
---
+++ plugin/lspconfig.lua
@@ -0,0 +1,36 @@
+local status, lsp = pcall(require, 'lspconfig')
+
+if (not status) then return end
+
+local protocol = require('vim.lsp.protocol')
+
+local on_attach = function(client, bufnr)
+  -- Formating
+  if client.server_capabilities.documentFormattingProvider then
+    print('on attach')
+    vim.api.nvim_command [[augroup Format]]
+    vim.api.nvim_command [[autocmd! * <buffer>]]
+    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    vim.api.nvim_command [[augroup END]]
+  end
+end
+
+lsp.tsserver.setup {
+  on_attach = on_attach,
+  filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
+  cmd = { 'typescript-language-server', '--stdio' }
+}
+
+lsp.sumneko_lua.setup {
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true)
+      }
+    }
+  }
+}
